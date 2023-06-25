8424a379c32138613aedfdb554f9cb04e3b3b493
Permyakov-Kirill <115360374+Permyakov-Kirill@users.noreply.github.com>
2023-06-25 20:52:32 +0300

    Update 3.6.5.jl

--- 3.6.5.jl
+++ 3.6.5.jl
@@ -1,16 +1,16 @@
 function meanpath(tree:Tree)
   
-    function sumpath_numver(tree::Tree)
-        n = 1
-        s = 0
-        for sub in tree.sub
-            s, n = sumpath_numver(sub)
-            s += s 
-            n += n 
-        end
-        return s, n 
+  function sumpath_numver(tree::Tree)
+    n = 1
+    s = 0
+    for sub in tree.sub
+      s, n = sumpath_numver(sub)
+      s += s 
+      n += n 
     end
+    return s, n 
+  end
 
-    s, n = sumpath_numver()
-    return s / n
+  s, n = sumpath_numver()
+  return s / n
 end
