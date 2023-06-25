175bfa02fdfa08a6edfdfa7a32177792aaa43e31
Permyakov-Kirill <115360374+Permyakov-Kirill@users.noreply.github.com>
2023-06-25 20:51:08 +0300

    Update 3.6.3.jl

--- 3.6.3.jl
+++ 3.6.3.jl
@@ -1,7 +1,7 @@
 function vernumber(tree::Tree)
-    n = 0
-    for sub in tree.sub
-        n += vernumber(sub)
-    end
-    return n + 1
+  n = 0
+  for sub in tree.sub
+    n += vernumber(sub)
+  end
+  return n + 1
 end
