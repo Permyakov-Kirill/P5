bb67f20e3f3f83f5201bf8439b1c2ecffa0532b0
Permyakov-Kirill <115360374+Permyakov-Kirill@users.noreply.github.com>
2023-06-25 20:55:06 +0300

    Update 4.5.jl

--- 4.5.jl
+++ 4.5.jl
@@ -1,28 +1,28 @@
 using LinearAlgebra
 
 function transform_to_steps!(A::AbstractMatrix; epsilon = 1e-7)
-    @inbounds for k in 1:size(A, 1)
-        absval, delta_k = findmax(abs, @view(A[k:end, k]))
-        (absval <= epsilon) && throw("Вырожденая матрица")
-        delta_k > 1 && swap!(@view(A[k, k:end]), @view(A[k + delta_k - 1, k:end]))
-        for i in k + 1:size(A, 1)
-            t = A[i, k] / A[k, k]
-            @. @views A[i, k:end] = A[i, k:end] - t * A[k, k:end]
-        end
+  @inbounds for k in 1:size(A, 1)
+    absval, delta_k = findmax(abs, @view(A[k:end, k]))
+    (absval <= epsilon) && throw("Вырожденая матрица")
+    delta_k > 1 && swap!(@view(A[k, k:end]), @view(A[k + delta_k - 1, k:end]))
+    for i in k + 1:size(A, 1)
+      t = A[i, k] / A[k, k]
+      @. @views A[i, k:end] = A[i, k:end] - t * A[k, k:end]
     end
-    return A 
+  end
+  return A 
 end
 
 
 @inline function swap!(A, B)
-    @inbounds for i in eachindex(A)
-        A[i], B[i] = B[i], A[i]
-    end
+  @inbounds for i in eachindex(A)
+    A[i], B[i] = B[i], A[i]
+  end
 end
 
 
 function random_upper_triagular(N::Integer)
-    A = randn(N, N)
-    _, A = lu(A)
-    return A 
+  A = randn(N, N)
+  _, A = lu(A)
+  return A 
 end
