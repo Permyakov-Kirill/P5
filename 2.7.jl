f7d593b9603631ea7d0df5d2a7901eb0ee7d5a5c
Permyakov-Kirill <115360374+Permyakov-Kirill@users.noreply.github.com>
2023-06-25 20:43:49 +0300

    Update 2.7.jl

--- 2.7.jl
+++ 2.7.jl
@@ -1,12 +1,12 @@
 function newton(r::Function, x, epsilon = 1e-8; num_max = 10)
-    dx = -r(x)
-    k=0
-    while abs(dx) > epsilon && k <= num_max
-        x += dx
-        k += 1
-    end
-    k > num_max && @warn("Требуемая точность не достигнута")
-        return 2
+  dx = -r(x)
+  k=0
+  while abs(dx) > epsilon && k <= num_max
+    x += dx
+    k += 1
+  end
+  k > num_max && @warn("Требуемая точность не достигнута")
+    return 2
   return 1
 end
 
