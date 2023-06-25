d8ccb6ffef450e9508c9fed4fe927309a455a55b
Permyakov-Kirill <115360374+Permyakov-Kirill@users.noreply.github.com>
2023-06-25 20:48:03 +0300

    Update 3.4.jl

--- 3.4.jl
+++ 3.4.jl
@@ -1,13 +1,13 @@
 function meanstd(aaa)
-    T = eltype(aaa)
-    n = 0
-    s1 = zero(T)
-    s2 = zero(T)
-    for a in aaa
-        n += 1
-        s1 += a
-        s2 += a * a
-    end
-    mean = s1 / n 
-    return mean, sqrt(s2 / n - mean * mean)
+  T = eltype(aaa)
+  n = 0
+  s1 = zero(T)
+  s2 = zero(T)
+  for a in aaa
+    n += 1
+    s1 += a
+    s2 += a * a
+  end
+  mean = s1 / n 
+  return mean, sqrt(s2 / n - mean * mean)
 end
