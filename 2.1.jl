f69caf47005bf475b198e7df9fbd3f1c5d6dc6ad
Permyakov-Kirill <115360374+Permyakov-Kirill@users.noreply.github.com>
2023-06-25 20:34:26 +0300

    Update 2.1.jl

--- 2.1.jl
+++ 2.1.jl
@@ -1,16 +1,16 @@
 function pow(a, n)
-  	b = a
-    p = 1
-    k = n
-  
-    while k > 0
-    		if k % 2 === 0
-      			k = k / 2
-      			b = b * b
-         else
-      			k = k - 1
-      			p = p * b
-        end
-		end
-  	return p
+  b = a
+  p = 1
+  k = n
+
+  while k > 0
+    if k % 2 === 0
+      k = k / 2
+      b = b * b
+     else
+      k = k - 1
+      p = p * b
+    end
+  end
+  return p
 end
