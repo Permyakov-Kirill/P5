87c2adb28945371e119970f5e80f2bb986f5497f
Permyakov-Kirill <115360374+Permyakov-Kirill@users.noreply.github.com>
2023-06-25 20:49:05 +0300

    Update 3.5.jl

--- 3.5.jl
+++ 3.5.jl
@@ -3,12 +3,12 @@ NestedVectors = Vector
 
 
 function convert_to_nested(tree::ConnectList{T}, root::T) where T 
-    nested_tree = []
-    for subroot in tree[root]
-        push!(nested_tree, convert(tree, subroot))
-    end
-    push!(nested_tree, root)
-    return nested_tree
+  nested_tree = []
+  for subroot in tree[root]
+    push!(nested_tree, convert(tree, subroot))
+  end
+  push!(nested_tree, root)
+  return nested_tree
 end
 
 
@@ -17,19 +17,19 @@ nested_tree = convert_to_nested(tree, 1)
 
 
 function convert_to_dict(tree::NestedVectors)
-    T = typeof(tree[end])
-    connect_tree = Dict{T, Vector{T}}()
+  T = typeof(tree[end])
+  connect_tree = Dict{T, Vector{T}}()
 
-    function recurs_trace(tree)
-        connect_tree[tree[end]] = []
-        for subtree in tree[1:end-1]
-            push!(connect_tree[tree[end]], resurs_trace(subtree))
-        end
-        return tree[end]
+  function recurs_trace(tree)
+    connect_tree[tree[end]] = []
+    for subtree in tree[1:end-1]
+      push!(connect_tree[tree[end]], recurs_trace(subtree))
     end
+    return tree[end]
+  end
 
-    recurs_trace(tree)
-    return connect_tree
+  recurs_trace(tree)
+  return connect_tree
 end
 
 
@@ -48,11 +48,11 @@ tree = [ [2, 3], [], [4, 5], [], [] ]
 
 
 function convert_to_list(tree::Dict{T, Vector{T}}) where T 
-    list_tree = Vector{Vector{T}}(undef, length(tree))
-    for subroot in eachindex(list_tree)
-        list_tree[subroot] = tree[subroot]
-    end
-    return list_tree
+  list_tree = Vector{Vector{T}}(undef, length(tree))
+  for subroot in eachindex(list_tree)
+    list_tree[subroot] = tree[subroot]
+  end
+  return list_tree
 end
 
 list_tree = convert_to_list(dict_tree)
