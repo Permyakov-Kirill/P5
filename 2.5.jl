56ea3b163657a17585389cc7ffd332835b8b2faf
Permyakov-Kirill <115360374+Permyakov-Kirill@users.noreply.github.com>
2023-06-25 20:37:54 +0300

    Update 2.5.jl

--- 2.5.jl
+++ 2.5.jl
@@ -1,19 +1,18 @@
 function bisection(f::Function, a, b, epsilon=1e-3)
-    f_a = f(a)
-    while b - a > epsilon
-        t = (a + b) / 2
-        f_t = f(t)
-        if f_t == 0
-            return t
-        end
-        if f_a * f_t < 0
-            b = t
-        else
-            a, f_a = t, f_t
-        end
+  f_a = f(a)
+  while b - a > epsilon
+    t = (a + b) / 2
+    f_t = f(t)
+    if f_t == 0
+      return t
     end
-    return (a + b) / 2
+    if f_a * f_t < 0
+      b = t
+    else
+      a, f_a = t, f_t
+    end
+  end
+  return (a + b) / 2
 end
-
 
 print(bisection(x->cos(x) - x, 3.2, 1, 7))
