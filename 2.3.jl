074258aa2b9c64d73acb772f8a10929cde520224
Permyakov-Kirill <115360374+Permyakov-Kirill@users.noreply.github.com>
2023-06-25 20:36:14 +0300

    Update 2.3.jl

--- 2.3.jl
+++ 2.3.jl
@@ -1,19 +1,19 @@
 function log(x, a, eps)
-  	y = 0
-  	z = x
-  	t = 1
+  y = 0
+  z = x
+  t = 1
 
-  	while abs(t) >= eps || z <= 1 / a || z >= a
-    		if z >= a
-      			z = z / a
-      			y = y + t
-    		elseif z <= 1 / a
-      			z = z * a
-      			y = y - t
-    		else
-      			z = z * z
-      			t = t / 2
-    		end
-  	end
-  	return y
+  while abs(t) >= eps || z <= 1 / a || z >= a
+    if z >= a
+      z = z / a
+      y = y + t
+    elseif z <= 1 / a
+      z = z * a
+      y = y - t
+    else
+      z = z * z
+      t = t / 2
+    end
+  end
+  return y
 end
